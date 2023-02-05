5f7b85979f1376678f8c5375a6e046708a4c024c
František Poboček <pobocekfrantisek@gmail.com>
2023-02-05 16:48:31 +0100

    Custom router & findBySlug

--- cms/src/api/post/controllers/post.ts
+++ cms/src/api/post/controllers/post.ts
@@ -2,6 +2,20 @@
  * post controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::post.post');
+export default factories.createCoreController('api::post.post', ({ strapi }) => ({
+	async findOneBySlug(ctx) {
+		const { slug } = ctx.params;
+
+		const query = {
+			filters: { slug },
+			...ctx.query
+		};
+
+		const post = await strapi.entityService.findMany('api::post.post', query);
+		const sanitizedEntity = await this.sanitizeOutput(post);
+
+		return this.transformResponse(sanitizedEntity[0]);
+	}
+}));
--- cms/src/api/post/routes/post.ts
+++ cms/src/api/post/routes/post.ts
@@ -1,7 +1,15 @@
-/**
- * post router
- */
-
 import { factories } from '@strapi/strapi';
+import customRouter from '../../../utils/customRouter';
 
-export default factories.createCoreRouter('api::post.post');
+const defaultRouter = factories.createCoreRouter('api::post.post');
+
+const customRoutes = [
+	{
+		method: 'GET',
+		path: '/posts/slug/:slug',
+		handler: 'post.findOneBySlug',
+		config: { policies: [] }
+	}
+];
+
+export default customRouter(defaultRouter, [], customRoutes);
