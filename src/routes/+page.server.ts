859102e9e045d1d842f93de762cd95991defad84
František Poboček <pobocekfrantisek@gmail.com>
2023-04-26 19:09:10 +0200

    sort recent

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,7 +6,7 @@ const baseUrl = CMS_BASE_API_URL + '/posts';
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	const http = await fetch(
-		`${baseUrl}?populate=tags,feature_img&filters[type][name][$eq]=project&filters[featured][$eq]=true`
+		`${baseUrl}?populate=tags,feature_img&filters[type][name][$eq]=project&filters[featured][$eq]=true&sort[0]=publishedAt:desc`
 	);
 	const res = await http.json();
 
